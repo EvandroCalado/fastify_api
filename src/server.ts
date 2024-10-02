7a1986fd258435d46ffa9cea867969625f4f3667
Evandro Calado <evandrocalado07@gmail.com>
2024-10-02 09:21:22 -0300

    chore: add eslit

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import fastify from 'fastify';
 const app = fastify();
 
 app.get('/', async (request, reply) => {
-	return { hello: 'world' };
+  return { hello: 'world' };
 });
 
 app.listen({ port: 3333 }).then(() => {
-	console.log('Server is running!');
+  console.log('Server is running!');
 });
