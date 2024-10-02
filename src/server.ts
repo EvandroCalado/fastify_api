8d7aa907ab8767e533c4cd994bc1e2bf04b576e4
Evandro Calado <evandrocalado07@gmail.com>
2024-10-02 09:32:58 -0300

    chore: add knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify';
+import { knex } from './database';
 
 const app = fastify();
 
 app.get('/', async (request, reply) => {
-  return { hello: 'world' };
+  const test = await knex('sqlite_schema').select('*');
+
+  return test;
 });
 
 app.listen({ port: 3333 }).then(() => {
