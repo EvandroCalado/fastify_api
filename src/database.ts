9fb84b6f1eb9831cb9b52343736517df97fef18c
Evandro Calado <evandrocalado07@gmail.com>
2024-10-02 10:01:02 -0300

    chore: config knex migrations

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,15 @@
-import { knex as setupKnex } from 'knex';
+import { Knex, knex as setupKnex } from 'knex';
 
-export const knex = setupKnex({
+export const knexConfig: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './database.sqlite',
+    filename: './database/db.sqlite',
   },
   useNullAsDefault: true,
-});
+  migrations: {
+    extension: 'ts',
+    directory: './database/migrations',
+  },
+};
+
+export const knex = setupKnex(knexConfig);
