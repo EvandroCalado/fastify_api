7f14fce3440dd814512f36fccc2ab442ed61f919
Evandro Calado <evandrocalado07@gmail.com>
2024-10-03 08:02:49 -0300

    feat: create get all, get one and summary

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,27 @@ import { z } from 'zod';
 import { knex } from '../database';
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  // get all transactions
+  app.get('/', async (request, reply) => {
+    const transactions = await knex('transactions').select();
+
+    return reply.status(200).send({ transactions });
+  });
+
+  // get one transaction
+  app.get('/:id', async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(request.params);
+
+    const transaction = await knex('transactions').where({ id }).first();
+
+    return reply.status(200).send({ transaction });
+  });
+
+  // create transation
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -22,5 +43,14 @@ export async function transactionsRoutes(app: FastifyInstance) {
     });
 
     return reply.status(201).send('Transaction created');
+  });
+
+  // summary
+  app.get('/summary', async (request, reply) => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first();
+
+    return reply.status(200).send({ summary });
   });
 }
